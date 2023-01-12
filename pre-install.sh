4613664e21fd53a50a61b845e3556d6434580e77
cbot918 <cbot918@gmail.com>
2023-01-12 21:30:48 +0800

    update pre-install.sh: create pre_config

--- pre-install.sh
+++ pre-install.sh
@@ -1,6 +1,22 @@
-pre_install(){
+pre_config(){
+
 	git config --global user.name "cbot918"
 	git config --global user.email "cbot918@gmail.com"
+
+	echo '
+	if [ -f ~/.yale/.bash_yale ]; then
+			. ~/.yale/.bash_yale
+	fi
+	' >> ~/.bashrc
+	source ~/.bashrc
+
+}
+pre_config
+
+
+
+pre_install(){
+
 	sudo apt update
 	sudo apt install make
 }
---
+++ test.sh
@@ -0,0 +1,6 @@
+echo '
+if [ -f ~/.yale/.bash_yale ]; then
+		. ~/.yale/.bash_yale
+fi
+' >> a.txt
+
