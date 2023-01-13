e58d1def9e1edcd5f194d0fb74a43530011e0387
cbot918 <cbot918@gmail.com>
2023-01-13 22:56:29 +0800

    update-minikube-instlal

--- Makefile
+++ Makefile
@@ -2,10 +2,8 @@ dev-env:
 	./src/dev-env/docker-install.sh
 	./src/dev-env/lang-install.sh
 
-
-# working on
-# init-minikube:
-# 	echo "implementing"
+init-mini:
+	./src/minikube/minikube-install.sh
 
 # init-dock:
 # 	echo "ToBeImpl"
--- src/minikube/minikube-install.sh
+++ src/minikube/minikube-install.sh
@@ -1,4 +1,26 @@
-minikube-install(){
-    echo hi
+minikube_install(){
+    minikube
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+    echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+    rm minikube-linux-amd64
+    
+    # kubectl
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    rm kubectl
+
+    # echo 'alias mini="minikube "' >> ~/.bashrc
+    # echo 'alias kubc="kubectl "' >> ~/.bashrc
+
+    echo ''
+    echo '-'
+    echo ''
+    echo 'todo:'
+    echo 'source ~/.bashrc'
+    echo 'test with $ minikube'
+    echo 'test with $ kubectl'
+
 }
-minikube-install
+minikube_install
+
