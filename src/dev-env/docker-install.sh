0eed0a710362366a469c0848a13486e8e05aa380
cbot918 <cbot918@gmail.com>
2023-01-14 07:18:19 +0800

    update+change+docker+install

--- src/dev-env/docker-install.sh
+++ src/dev-env/docker-install.sh
@@ -1,5 +1,6 @@
 docker_install(){
     # sudo apt install build-essential
-    sudo apt install docker.io -y
+    #sudo apt install docker.io -y
+		sudo apt install docker-ce docker-ce-cli containerd.io -y
 }
 docker_install
