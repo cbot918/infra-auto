20b9be6506a14004c9373dd341d6b7ce1550e612
cbot918 <cbot918@gmail.com>
2023-01-25 17:34:48 +0800

    nvim.sh done

--- src/.yale/vim/nvim.sh
+++ src/.yale/vim/nvim.sh
@@ -1,29 +1,28 @@
-DNAME=test
-EXECC=exec
+CNAME=nvim-test                             # public_config: set container name
+EXECC=exec                                  # private_config: because exec is shell's reserved word 
+docker run -dit --name $CNAME ubuntu        # create a container with image:ubuntu
+docker $EXECC $CNAME sh -c 'apt update && apt install git curl neovim -y' # install package
 
-# create a container with image:ubuntu
-docker run -dit --name $DNAME ubuntu
 
-# basic install
-# docker $EXECC $DNAME sh -c ' apt update && apt install git tmux golang-go curl -y'
-
-# nvim install/setup
-docker $EXECC $DNAME sh -c '
+## init nvim required config folder ( init.vim is vim config )
+docker $EXECC $CNAME sh -c '
 mkdir -p ~/.config/nvim && 
 touch ~/.config/nvim/init.vim && 
 mkdir -p ~/.vim/autoload
 '
 
-docker $EXECC $DNAME sh -c 'curl -OL '
 
-# let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
-# if empty(glob(data_dir . '/autoload/plug.vim'))
-#   silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-#   autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
-# endif
+## down pre-setup to init.vim
+docker $EXECC $CNAME sh -c 'curl -o ~/.config/nvim/init.vim -OL https://raw.githubusercontent.com/cbot918/infra-auto/main/src/.yale/vim/init.vim'
 
 
+echo "
+enter and test: 
+$ docker exec -it nvim-test bash
+$ nvim
+"
 
 
-docker container stop $DNAME
+## teardown code for use :)
+# docker container stop $DNAME
 # docker container rm $DNAME
