23af4fbaea266a39c24a4ea1695b3cf429020852
cbot918 <cbot918@gmail.com>
2023-02-09 03:01:35 +0000

    update

--- src/dev-env/arm-install.sh
+++ src/dev-env/arm-install.sh
@@ -1,5 +1,5 @@
 arm(){
 	sudo apt update
-	sudo apt install qemu-system-arm libncurses5-dev gcc-aarch64-linux-gnu build-essential git bison flex libssl-dev
+	sudo apt install qemu-system-arm libncurses5-dev gcc-aarch64-linux-gnu build-essential git bison flex libssl-dev -y
 }
 arm
