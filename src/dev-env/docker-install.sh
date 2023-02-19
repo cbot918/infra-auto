e32e53a94fb150fbcecfd9821cc9aa8a1cbc460b
cbot918 <cbot918@gmail.com>
2023-02-19 15:09:01 +0800

    update src/dev-env/docker-install.sh: fix docker-compose install version

--- src/dev-env/docker-install.sh
+++ src/dev-env/docker-install.sh
@@ -20,6 +20,8 @@ docker_install(){
 		sudo apt-get install docker-ce docker-ce-cli containerd.io -y
     sudo docker run hello-world
 		sudo gpasswd -a $USER docker
-    sudo apt install docker-compose -y
+    sudo curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 }
 docker_install
