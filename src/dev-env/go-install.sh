cfb6bee11f5fab7e36d382eac4efd45b6cbee165
cbot918 <cbot918@gmail.com>
2023-02-20 02:03:22 +0800

    update

--- src/dev-env/go-install.sh
+++ src/dev-env/go-install.sh
@@ -5,7 +5,7 @@ install_go(){
     sudo rm -rf /usr/local/go
     sudo tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
     rm $BINARY
-    echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
+    echo "export PATH=\$PATH:/usr/local/go/bin" >> $HOME/.bashrc
     source $HOME/.bashrc
     echo "[*] auto check go-version"
     go version
