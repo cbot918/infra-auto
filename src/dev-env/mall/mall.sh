cb2b4d4483a93eb62048a4ca91450a006a775333
cbot918 <cbot918@gmail.com>
2023-09-02 20:07:08 +0800

    update

--- src/dev-env/mall/mall.sh
+++ src/dev-env/mall/mall.sh
@@ -1,4 +1,13 @@
 mall(){
+
+  sudo cp document/docker/nginx.conf /mydata/nginx/
+
+  sudo mkdir /mydata/mysql/conf/mysql.conf.d
+  sudo mkdir /mydata/mysql/conf/conf.d
+
   docker-compose -f docker-compose-env.yml up
+
+  
+
 }
 mall
---
+++ src/dev-env/mall/premall.sh
@@ -0,0 +1,4 @@
+premall(){
+
+}
+premall
